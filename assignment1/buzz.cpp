ac6b51d3454a0a368b77a35c34013412946c71eb
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-02 21:45:13 +0530

    Create buzz.cpp

---
+++ assignment1/buzz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+main() 
+{
+    int n;
+    cout<<"Enter a number";
+    cin>>n;
+    if(n%7==0 || n%10==7)
+    cout<<"buzz number";
+    else
+    cout<<" not buzz number";
+}
