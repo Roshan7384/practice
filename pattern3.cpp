d4c2a7567efc8162f308da714902ebe944539608
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-04 14:31:23 +0530

    Create pattern3.cpp

---
+++ pattern3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;++i)
+    {
+        for(j=1;j<=4;++j)
+        {
+        
+        cout<<i;
+        }
+    cout<<"\n";
+    }
+    
+}
