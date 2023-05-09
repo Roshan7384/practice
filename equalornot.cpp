70481edd18e5494e45c1d505e94f8a1b4c60a4bc
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-09 22:56:33 +0530

    equalornot.cpp

---
+++ equalornot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+    if(a==b)
+    cout<<"equal";
+    else
+    cout<<"not equal";
+}
+    
