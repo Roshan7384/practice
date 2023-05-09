0c8c509fa433e317087830bf164f1dcb6eb14b18
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-09 22:51:06 +0530

    areacir.cpp

---
+++ areacir.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r,area,cir;
+    cout<<"enter radius of circle";
+    cin>>r;
+    area=3.14*r*r;
+    cir=2*3.14*r;
+    cout<<area<<endl;
+    cout<<cir;
+}
+    
