e96b0ef1f348b9c8e0fe2c46cc3139853ccb48b7
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-09 22:59:54 +0530

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a numbers";
+    cin>>a;
+    if(a%2==0)
+    {
+      cout<<"even";
+    }
+    else
+    {
+      cout<<"odd";
+    }
+}
+    
