caf57867a0af88f69eaa106709835c46ff1485ac
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-26 11:43:28 +0530

    Create fucction.cpp

---
+++ fucction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int sum(int x, int y){
+  // int c;
+  // c=x+y;
+  return x+y;
+}
+int main() 
+{
+  int a,b;
+  cin>>a>>b;
+  cout<<sum(a,b); 
+   
+}
+
