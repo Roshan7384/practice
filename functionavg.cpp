6c55fdd51a70026aa7f11294d54509699a4ee545
Roshan7384 <131859203+Roshan7384@users.noreply.github.com>
2023-05-26 11:59:31 +0530

    Create functionavg.cpp

---
+++ functionavg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+float avg(float x, float y){
+  // int c;
+  // c=x+y;
+  return (x+y)/2;
+}
+int main() 
+{
+  float a,b;
+  cin>>a>>b;
+  cout<<avg(a,b); 
+   
+}
